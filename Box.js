539addf869c80c9c008d5388b40a9114fd382a13
valurie3 <valurie3@gmail.com>
2026-02-16 12:30:15 -0600

    Delete Box.js

--- Box.js
+++
@@ -1,9 +0,0 @@
-<script>
-    function openNews() {
-        document.getElementById('news-deck').style.display = 'flex';
-    }
-
-    function closeNews() {
-        document.getElementById('news-deck').style.display = 'none';
-    }
-</script>
