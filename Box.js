bde9c30b3d63266e85f751e0ced2dcec100c24b3
valurie3 <valurie3@gmail.com>
2026-02-16 12:23:14 -0600

    Create Box.js

---
+++ Box.js
@@ -0,0 +1,8 @@
+function openNews() {
+    document.getElementById('news-deck').style.display = 'flex';
+}
+
+function closeNews() {
+    document.getElementById('news-deck').style.display = 'none';
+}
+
